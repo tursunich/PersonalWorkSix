d2ac89d8bcdca61ea481448d69cf3b20554402d1
tursunich <45151634+tursunich@users.noreply.github.com>
2018-11-20 10:20:38 +0600

    Update BooksAdapter.java

--- app/src/main/java/kz/education/kstu/personalworksix/BooksAdapter.java
+++ app/src/main/java/kz/education/kstu/personalworksix/BooksAdapter.java
@@ -45,44 +45,21 @@ public class BooksAdapter extends ArrayAdapter<Book> implements View.OnClickList
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         Book book = getItem(position);
-        ViewHolder viewHolder;
-
-        //final View result;
-
-        //if (convertView == null){
-
-            viewHolder = new ViewHolder();
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.list_item, parent, false);
-            viewHolder.title = (TextView) convertView.findViewById(R.id.title);
-            viewHolder.title.setText(book.getVolumeInfo().getTitle());
-            viewHolder.publisher = (TextView) convertView.findViewById(R.id.publisher);
-            viewHolder.publisher.setText(getFormattedString(R.string.publisher, book.getVolumeInfo().getPublisher() != null ? book.getVolumeInfo().getPublisher() : ""));
-            viewHolder.publishDate = (TextView) convertView.findViewById(R.id.publishDate);
-            viewHolder.publishDate.setText(getFormattedString(R.string.publishDate, book.getVolumeInfo().getPublishDate() != null ? book.getVolumeInfo().getPublishDate() : ""));
-            viewHolder.price = (TextView) convertView.findViewById(R.id.price);
+        ViewHolder viewHolder = new ViewHolder();
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        convertView = inflater.inflate(R.layout.list_item, parent, false);
+        viewHolder.title = (TextView) convertView.findViewById(R.id.title);
+        viewHolder.title.setText(book.getVolumeInfo().getTitle());
+        viewHolder.publisher = (TextView) convertView.findViewById(R.id.publisher);
+        viewHolder.publisher.setText(getFormattedString(R.string.publisher, book.getVolumeInfo().getPublisher() != null ? book.getVolumeInfo().getPublisher() : ""));
+        viewHolder.publishDate = (TextView) convertView.findViewById(R.id.publishDate);
+        viewHolder.publishDate.setText(getFormattedString(R.string.publishDate, book.getVolumeInfo().getPublishDate() != null ? book.getVolumeInfo().getPublishDate() : ""));
+        viewHolder.price = (TextView) convertView.findViewById(R.id.price);
         if (book.getSaleInfo().getListPrice() != null) {
             String amount = String.valueOf(book.getSaleInfo().getListPrice().getAmount());
             viewHolder.price.setText(getFormattedString(R.string.price, amount + " " + book.getSaleInfo().getListPrice().getCurrency()));
         }
-            //result = convertView;
-
-            convertView.setTag(viewHolder);
-//        } else {
-//            viewHolder = (ViewHolder) convertView.getTag();
-//            result = convertView;
-//        }
-//
-//        Animation animation = AnimationUtils.loadAnimation(mContext, (position > lastPosition) ? R.anim.up_from_bottom : R.anim.down_from_top);
-//        result.startAnimation(animation);
-//        lastPosition = position;
-//
-//        viewHolder.txtName.setText(dataModel.getName());
-//        viewHolder.txtType.setText(dataModel.getType());
-//        viewHolder.txtVersion.setText(dataModel.getVersion_number());
-//        viewHolder.info.setOnClickListener(this);
-//        viewHolder.info.setTag(position);
-//        // Return the completed view to render on screen
+        convertView.setTag(viewHolder);
         return convertView;
     }
 }
